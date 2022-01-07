f32526f33d342608ddf686e126563af83ba3f209
Score2 <836696023@qq.com>
2022-01-07 13:11:47 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("io.izzel.taboolib") version "1.34"
+}
+
+taboolib {
+    install("common")
+    install("common-5")
+    install(
+        "module-configuration",
+        "module-chat",
+        "module-lang",
+        "module-kether",
+        "module-effect",
+        "module-database",
+        "module-nms", // bukkit
+        "module-nms-util",
+        "module-ui",
+        "module-ai",
+        "module-porticus", // bungee
+    )
+    install("expansion-player-database", "expansion-command-helper")
+    install(
+        "platform-bukkit",
+        "platform-bungee"
+    )
+
+    classifier = null
+    version = "6.0.7-15"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.tabooproject.org/repository/releases") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
